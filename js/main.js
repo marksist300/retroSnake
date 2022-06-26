14301c687c735a38f2140a893bbd61428c80c106
Mark Linn <marktlinn@gmail.com>
2022-06-26 08:50:11 +0200

    initialised Snake object

--- js/main.js
+++ js/main.js
@@ -1,15 +1,41 @@
 let totalFoodEaten = 0;
 let size = 10
-const gameScreen = document.querySelector('.phone');
+const gameScreen = document.querySelector('canvas');
+
+let snake= {
+    _x: Math.floor(Math.random()*gameScreen.width),
+    _y: 2,
+    _width: 10,
+    _height: 5,
+
+    get xPos(){
+        return this._x;
+    }, 
+
+    get yPos(){
+        return this_y;
+    },
+
+    get width(){
+        return this._width;
+    },
+
+    get height(){
+        return this.height;
+    },
+
+    set width(value){
+        return this._width += value;
+    },
+}
 
 function gameCanvas(){
         console.log('loaded')
         const canvas = document.querySelector('#game-canvas');
         const ctx = canvas.getContext('2d');
         ctx.beginPath()
-        ctx.fillRect(2, 2, size, 5);
-        ctx.lineTo(50, 50);
-        ctx.stroke()
+        // ctx.fillRect(snake.x, snake.y, snake.width, snake.height);
+        // ctx.lineTo(50, 50);
         // filled square X: 125, Y: 35, width/height 50
     }
 gameCanvas()
@@ -17,7 +43,8 @@ gameCanvas()
 gameScreen.addEventListener('click', increaser)
 
 function increaser(){
-    size += 5;
+    snake.width += 5;
     console.log(size)
     gameCanvas()
 }
+console.log()
