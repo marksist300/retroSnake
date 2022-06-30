ca95d1b2fe39ae9c5d8979361b5c9b156b736b02
Mark Linn <marktlinn@gmail.com>
2022-06-30 20:35:57 +0200

    got a functioning snake that grows and moves correctly, need to adjust the speed of growth: make it faster; still an issue withe food eating recognition

--- js/main.js
+++ js/main.js
@@ -5,27 +5,71 @@ const ctx = canvas.getContext('2d');
 function randomGenerationPoint(min, max) { // min and max included 
     return Math.floor(Math.random() * (max - min + 1) + min)
   }
+
+class SnakePart{
+    constructor(x,y){
+        this.x = x;
+        this.y= y;
+    }
+}
   
 let speed = 7;
+let headY = randomGenerationPoint(1, canvas.height);
+let headX = randomGenerationPoint(1, canvas.width);
+let moveX = 0;
+let moveY = 0;
+let foodY = randomGenerationPoint(1, canvas.height);
+let foodX = randomGenerationPoint(1, canvas.width);
+const snakeBody = [];
+let tailLength = 2;
 
 function randomGenerationPoint(min, max) { // min and max included 
     return Math.floor(Math.random() * (max - min + 1) + min)
   }
 
 function drawGame(){
-    setTimeout(drawGame, 1000/speed)
+    clearScreen()
+    moveSnake()
+    snakeEatsFood()
+    drawFood()
+    drawSnake()
+    setTimeout(drawGame, 100/speed)
 }
 
 function clearScreen(){
-    ctx.clearRect(0,0, gameScreen.width, gameScreen.height)
+    ctx.clearRect(0,0, canvas.width, canvas.height)
 }
 
+function drawSnake(){
 
-headY = randomGenerationPoint(1, canvas.height);
-headX   = randomGenerationPoint(1, canvas.width);
-moveX = 0;
-moveY = 0;
+    for(let i = 0; i<snakeBody.length; i++){
+        let body = snakeBody[i];
+        ctx.fillRect(body.x, body.y, 5, 5)
+    }
 
+    snakeBody.push(new SnakePart(headX, headY))
+    if(snakeBody.length > tailLength) snakeBody.shift();
+
+    ctx.fillRect(headX, headY, 5, 5);
+
+}
+
+function moveSnake(){
+    headX += moveX;
+    headY += moveY;
+}
+
+function drawFood(){
+    ctx.fillRect(foodX, foodY, 4, 4)
+}
+
+function snakeEatsFood(){
+    if(foodX === headX && foodY === headY){
+        foodY = randomGenerationPoint(1, canvas.height);
+        foodX = randomGenerationPoint(1, canvas.width);
+        tailLength++;
+    }
+}
 //event listener for key presses
 document.body.addEventListener('keydown', e=>{
     e.preventDefault();
@@ -72,3 +116,4 @@ function positionKey(e){
 }
 
 
+drawGame()
